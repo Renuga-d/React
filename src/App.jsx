7ad31faf9c407c003aaf3120084949135d7da12c
RentalWebRenuga <https://github.com/Clanizon/RentalWebRenuga.git>
2022-01-05 14:14:51 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import f from './f.png'
 import Instagram from './Instagram logo.jpg'
 import twit from './twit.png'
 import Content from './Content.jsx'
+import Footer from './Footer.jsx'
 
 function App() {
   
@@ -14,28 +15,9 @@ function App() {
    
     <div className='container'>
       <Header />
-   <div className='div2'>
-       <img src={img} className='cares_image' />
-   </div>
- <p>We bring you stories along with graduate from the many drivers and <br></br>
- helpers of our partner bus operators, whose lives changed bacause of all<br></br>
- your contributions.</p>
-  <p>But as the lockdown continues, so do their struggles. And we hope you<br></br>
-  join our efforts in delivering essentials along with hope to them and their<br></br>
-  families. Let's hear from the drivers.</p>  
+       <Body />
        <Content />
-          <div className="footer">
-     <div className="left1">
-       Copyrights 2020
-     </div>
-     <div className="right1">
-       <ul>
-         <li><img src={f} className='images_footer' /></li>
-         <li><img src={Instagram}  className='images_footer' /></li>
-         <li><img src={twit} className='images_footer' /></li>
-       </ul>
-     </div>
-   </div>
+        <Footer />
    </div>
    
   )
---
+++ src/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <div className="left1">
+       Copyrights 2020
+     </div>
+     <div className="right1">
+       <ul>
+         <li><img src={f} className='images_footer' /></li>
+         <li><img src={Instagram}  className='images_footer' /></li>
+         <li><img src={twit} className='images_footer' /></li>
+       </ul>
+     </div>
+    </div>
+  )
+}
+
+export default Footer
